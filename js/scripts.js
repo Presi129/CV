cc56142e541c7d4e01c1c61f3ad8437429458c62
Damian Sieradzki <sieradzki129@gmail.com>
2020-09-23 05:57:08 +0200

    *

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,31 @@
-let url = window.location.href
+/*let url = window.location.href
 
 function highlightElements() {
 
     console.log(url);
 }
 
+*/
+function toggle() {
+    let navbar = document.querySelector("#navbarSupportedContent");
+    if (navbar.style.display === "none") {
+        navbar.style.display = "block";
+    }
+    else {
+        navbar.style.display = "none";
+    }
+}
+
+function switchLight() {
+
+
+    if (document.body.style.backgroundColor === "#ffffff") {
+        document.body.style.backgroundColor = "#18191A";
+    }
+    else {
+        document.body.style.backgroundColor = "#ffffff";
+    }
+}
 
 
 
