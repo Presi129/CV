d944b4f18a1bc351866809ff507e3ccebcb44f33
Damian Sieradzki <sieradzki129@gmail.com>
2021-01-04 22:50:38 +0100

    Updated

--- js/scripts.js
+++ js/scripts.js
@@ -28,7 +28,7 @@ var i = 0;
     function typeText() {
         const speed = 10;
         let txtClass = document.querySelector('.text-typing')
-        const txt = "I have about 1 years of experience in software development as Web Developer and Software Engineer in banking industry. I got experience with new functionalities and maintenance software. Last months I worked in Scrum teams in projects concerning banking website and integrate them with internal systems. I am oriented for further development as Front-end Developer";
+        const txt = "I have about 1.5 years of experience in software development as Web Developer and Software Engineer in banking industry. I got experience with new functionalities and maintenance software. Last months I worked in Scrum teams in projects concerning banking website and integrate them with internal systems. I am oriented for further development as Front-end Developer";
         if (i < txt.length) {
             txtClass.textContent += txt.charAt(i);
             i++;
